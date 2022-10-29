09b9bd5a5fe88c71faa8b72dc2eec64abd5c85c9
Tobias Reidarsen <tobias.reidarsen@gmail.com>
2022-10-29 23:27:42 +0200

    Made it so that the user can write text that is displayed, and be able to delete that texts and make a newLine using BackSpace.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿TxtEd();
 
-Console.WriteLine("Hello, World!");
+static void TxtEd()
+{
+    int lineNumber = 0;
+    Console.Write("#" + lineNumber + ": ");
+    int pos = Console.CursorLeft;
+    ConsoleKeyInfo info;
+    List<char> userTxt = new List<char>();
+    
+
+    while (true)
+    {
+        info = Console.ReadKey(true);
+            if (info.Key == ConsoleKey.Backspace && Console.CursorLeft > pos)
+            {
+                if (userTxt.Count > 0)
+                {
+                    userTxt.RemoveAt(userTxt.Count - 1);
+                    Console.CursorLeft -= 1;
+                    Console.Write(' ');
+                    Console.CursorLeft -= 1;
+                }
+            }
+            else if (info.Key == ConsoleKey.Enter)
+            {
+                Console.Write(Environment.NewLine);
+                lineNumber++;
+                Console.Write("#" + lineNumber + ": ");
+                
+            }
+            else if (Char.IsLetterOrDigit(info.KeyChar))
+            {
+                Console.Write(info.KeyChar);
+                userTxt.Add(info.KeyChar);
+            }
+    }
+}
+
+// Code used from here: https://stackoverflow.com/questions/7565415/edit-text-in-c-sharp-console-application
+// By user: Tadeusz
+/*
+string input = "her er en string";
+
+ReadLine(input);
+
+static string ReadLine(string input)
+{
+    int pos = Console.CursorLeft;
+    Console.WriteLine(input);
+    ConsoleKeyInfo info;
+    List<char> chars = new List<char>();
+    if (string.IsNullOrEmpty(input) == false)
+    {
+        chars.AddRange(input.ToCharArray());
+    }
+
+    while (true)
+    {
+        info = Console.ReadKey(true);
+        if (info.Key == ConsoleKey.Backspace && Console.CursorLeft > pos)
+        {
+            chars.RemoveAt(chars.Count - 1);
+            Console.CursorLeft -= 1;
+            Console.Write(' ');
+            //Console.CursorLeft -= 1;
+        }
+        else if (info.Key == ConsoleKey.Enter)
+        {
+            Console.Write(Environment.NewLine);
+        }
+        else if (char.IsLetterOrDigit(info.KeyChar))
+        {
+            Console.Write(info.KeyChar);
+            chars.Add(info.KeyChar);
+        }
+    }
+
+    return new string(chars.ToArray());
+}*/
